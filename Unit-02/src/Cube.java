f9ec96a8b41482196bd8292b28ea86b003fc3a0e
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-18 10:46:03 -0700

    unit-02 done

---
+++ Unit-02/src/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Arshia Nayebnazar
+//Date - 08/18/2022
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6 * Math.pow(side, 2);
+	}
+}
---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Arshia Nayebnazar
+//Date - 08/18/2022
+//Class - AP CSA
+//Lab  - Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double result =  (0.5 * (base1 + base2) * height);
+		return result;
+	}
+}
