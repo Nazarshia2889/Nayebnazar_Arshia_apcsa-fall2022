d45b4b7650c449d2976e195ec19c6fa023cdb76e
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-17 21:48:35 -0700

    unit-00

---
+++ Unit-00/src/AsciiArt.java
@@ -0,0 +1,32 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Arshia Nayebnazar
+//Date -08/17/2022
+//Class - AP CSA
+//Lab  - Ascii Art
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Arshia Nayebnazar \n\n" );
+		System.out.println("Dog" );
+		System.out.println("\n\n\n\n" );
+		
+		System.out.println("                          |------| ");
+		System.out.println("                          |   \"- | ");
+		System.out.println("    \\                     | |--- | ");
+		System.out.println("     |--------------------|------| ");
+		System.out.println("     |                    |        ");
+		System.out.println("     |--------------------|     ");
+		//add other output
+		System.out.println("   	|         |  ");
+		System.out.println("   	|         |  ");
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
