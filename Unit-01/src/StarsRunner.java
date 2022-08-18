e1b55071673b122853066d6b8eceb28560c9f8c6
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-17 22:11:58 -0700

    unit-01

---
+++ Unit-01/src/StarsRunner.java
@@ -0,0 +1,31 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   StarsAndStripes ref = new StarsAndStripes();
+      
+      //call the methods needed to make the patterns on the word document
+	   ref.printTwoBlankLines();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwoBlankLines();
+	   ref.printASmallBox();
+	   ref.printASmallBox();
+   }
+}
