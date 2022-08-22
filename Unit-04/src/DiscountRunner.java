47e04ee9b0c1d576156e9a8b22a143d65d7331f9
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-08-22 11:06:49 -0700

    unit-04

---
+++ Unit-04/src/DiscountRunner.java
@@ -0,0 +1,27 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+		
+		Discount discounter = new Discount();
+		double discount = discounter.getDiscountedBill(amt);
+		
+		System.out.println(discounter.toString(discount));
+	}
+}
